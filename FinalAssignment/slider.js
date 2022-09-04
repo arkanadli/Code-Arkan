a1725c7ef5fe58ff213bcb7591bfb1cdc5e7941b
arkanadli <uzzomaki@gmail.com>
2022-09-04 09:33:46 +0700

    final

---
+++ FinalAssignment/slider.js
@@ -0,0 +1,37 @@
+var slideIndex = 1;
+            showSlide(slideIndex);
+
+        function nextslide(n){
+            showSlide(slideIndex += n);
+        }
+
+        function dotslide(n){
+            showSlide(slideIndex = n);
+        }
+
+        function showSlide(n) {
+            var i;
+            var slides = document.getElementsByClassName("imgslide");
+            var dot = document.getElementsByClassName("dot");
+            
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            for (i = 0; i < slides.length; i++) {
+                
+                slides[i].style.display = "none";
+                dot[i].className = dot[i].className.replace(" active", "");
+            }
+
+        
+
+            slides[slideIndex - 1].style.display = "block"; //nampilin element slide[0]
+
+            dot[slideIndex - 1].className += " active";
+            
+
+
+        }
